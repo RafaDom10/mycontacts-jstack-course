0386fd139a8a62f3407002c2838a6329e854dab5
RafaDom10 <r-domingues@outlook.com>
2021-08-16 15:27:56 -0300

    added cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
+
+const port = 3001;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
+app.use(errorHandler);
 
-// Middleware errorHandler -> ordem necessária após routes
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
-
-app.listen(3000, () => console.log('> Server started at http://localhost:3000'));
+app.listen(port, () => console.log(`🔥 Server started at http://localhost:${port}`));
--- api/src/routes.js
+++ api/src/routes.js
@@ -5,16 +5,16 @@ const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
-router.post('/contacts', ContactController.store);
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
-router.put('/contacts/:id', ContactController.update);
 router.delete('/contacts/:id', ContactController.delete);
+router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
 
-router.post('/categories', CategoryController.store);
 router.get('/categories', CategoryController.index);
 router.get('/categories/:id', CategoryController.show);
-router.put('/categories/:id', CategoryController.update);
 router.delete('/categories/:id', CategoryController.delete);
+router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
